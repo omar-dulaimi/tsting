919acd126ab6f99c04550f023ea63c2f1d5382e3
MansourMoukdad88 <m_sy@outlook.com>
2018-12-03 10:33:09 +0200

    (feat) FrontEnd: add Teachers Components

---
+++ client/src/components/Teachers/Classes.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Classes extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Classes Component:</h1>
+            </div>
+        );
+    }
+
+}
+export default Classes;
