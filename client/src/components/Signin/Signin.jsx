3e4816aa36858f99523db43ebb2a0ba4597a6eeb
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-04 12:56:34 +0200

    (bug) center text element

--- client/src/components/Signin/Signin.jsx
+++ client/src/components/Signin/Signin.jsx
@@ -9,7 +9,7 @@ class Signin extends Component {
                 <div className="row">
                     <form className="form-signin col align-middle mt-5">
                         <span className="mbr-iconfont fa fa-user centerusericon mb-3" alt width={72} height={72} />
-                        <h1 className="h3 mb-5 font-weight-normal">Please sign in</h1>
+                        <h1 className="h3 mb-5 font-weight-normal centerusericon">Please sign in</h1>
                         <label htmlFor="inputEmail" className="sr-only">Email address</label>
                         <input type="email" id="inputEmail" className="form-control mb-4" placeholder="Email address" required autofocus />
                         <label htmlFor="inputPassword" className="sr-only">Password</label>
