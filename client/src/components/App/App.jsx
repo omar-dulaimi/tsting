1354fce0c67373fba5a531aaf5fc201c1b573639
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-04 12:19:40 +0200

    (feat) add sign in component

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -130,7 +130,7 @@ class App extends Component {
 
                 <ul className="navbar-nav ml-auto">
                   <li className="nav-item active">
-                    <a className="nav-link" data-toggle="modal" data-target="#signInModal" id="signinbtn">Sign In</a>
+                    <Link className="nav-link" to="/signin" id="signinbtn">Sign In</Link>
                   </li>
                 </ul>
               </div>
@@ -140,39 +140,9 @@ class App extends Component {
             {/* Routing */}
             <Route exact path="/" component={Home} />
             <Route exact path="/about" component={About} />
+            <Route exact path="/signin" component={Signin} />
           </div>
         </Router>
-
-
-        { /* Modal Sign In */}
-        <div className="modal fade" id="signInModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div className="modal-dialog modal-dialog-centered" role="document">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLongTitle">Welcome Back!</h5>
-                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                </button>
-              </div>
-              <div className="modal-body">
-                <div className="row">
-                  <div className="form-group col-xs-4 col-sm-6 col-md-8 col-lg-8">
-                    <input type="text" id="userNameSignInInput" className="form-control" placeholder="Username" required />
-                  </div>
-                </div>
-                <div className="row">
-                  <div className="form-group col-xs-4 col-sm-6 col-md-8 col-lg-8">
-                    <input type="password" id="passwordSignInInput" className="form-control" placeholder="Password" required />
-                  </div>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button type="button" className="btn btn-primary signinButton">Signin</button>
-                <button type="button" className="btn btn-secondary" data-dismiss="modal">Cancel</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        { /* Modal Sign In */}
 
 
         {/* Testing Components*/}
