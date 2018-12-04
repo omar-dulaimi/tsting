80c59fd2a355ab6f0f81b0cdc53130faa4ebaa27
omar-dulaimi <o.m.dulaimi@gmail.com>
2018-12-04 03:03:36 +0200

    (feat) Add calendar on home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import Calendar from 'react-calendar'
 
 class Home extends Component {
     state = {
+        date: new Date(),
         numberFact: ''
     }
 
@@ -19,14 +21,28 @@ class Home extends Component {
         return body;
     };
 
+    onChange = date => this.setState({ date })
+
     render() {
         return (
-            <div className="card border-primary mb-3 mt-3 container" style={{ width: '18rem' }}>
-                <div className="card-body text-primary">
-                    <h5 className="card-title">Fun Number Facts</h5>
-                    <p className="card-text">{this.state.numberFact}</p>
+            <div className="row">
+                <div className="col-md-2 ml-3">
+                    <div className="card border-primary mb-3 mt-3" style={{ width: '18rem', height: '350px' }}>
+                        <div className="card-body text-primary">
+                            <h5 className="card-title">Fun Number Facts</h5>
+                            <p className="card-text">{this.state.numberFact}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="col-md-8 mb-3 mt-3 ml-3">
+                    <Calendar
+                        onChange={this.onChange}
+                        value={this.state.date}
+                    />
                 </div>
             </div>
+
         );
     }
 }
